569f1aba2f31b24b4c127bf971030231ac33e833
VA-SA-NT-H <vasanthchandru08@gmail.com>
2025-06-21 19:26:17 +0530

    Binary search and Bubble sort added

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -1,13 +1,23 @@
 public class LinearSearch {
-    int nums[] = {12,34,45,56,67,78,89,90};
-    int target = 67;
-    int search(){
+    int[] nums;
+    int target;
+
+    public LinearSearch(int[] nums, int target) {
+        this.nums = nums;
+        this.target = target;
+    }
+
+    public int search() {
         int length = nums.length;
-        for(int i=0;i<length;i++){
-            if(nums[i]==target){
+        int steps = 0;
+        for (int i = 0; i < length; i++) {
+            steps++;
+            if (nums[i] == target) {
+                System.out.println("\nSteps taken by linear search : " + steps);
                 return i;
             }
         }
+        System.out.println("\nSteps taken by linear search : " + steps);
         return -1;
     }
 }
